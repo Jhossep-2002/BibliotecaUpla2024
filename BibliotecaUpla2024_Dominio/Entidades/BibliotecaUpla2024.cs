94bab209d72f339066efbecf96acf0f74975797c
ACER NITRO 5 <ACER NITRO 5@LAPTOP-A2AMA2PS>
2024-02-11 16:12:30 -0500

    Agregar archivos de proyecto.

---
+++ BibliotecaUpla2024_Dominio/Entidades/BibliotecaUpla2024.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaUpla2024_Dominio.Entidades
+{
+    public class BibliotecaUpla2024
+    {
+    }
+}
